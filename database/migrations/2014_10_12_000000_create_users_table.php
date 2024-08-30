bd80a2bc2ca27cf0a8497bc010384606f0d98cb8
Ibrahem-Khaled <xx.vvv48@gmail.com>
2024-08-30 17:04:24 +0200

    fix bug from add user dummy data

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -30,19 +30,22 @@ return new class extends Migration {
         });
 
         DB::table('users')->insert([
-            'name' => 'admin',
-            'phone' => '1234567890',
-            'email' => 'admin@admin.com',
-            'password' => bcrypt('12345678'),
-            'role' => 'admin',
-            'is_verified' => 'verified',
-        ], [
-            'name' => 'Noor qnebi',
-            'phone' => '0503889420',
-            'email' => 'noorqnebi@gmail.com	',
-            'password' => bcrypt('12345678'),
-            'role' => 'company',
-            'is_verified' => 'verified',
+            [
+                'name' => 'admin',
+                'phone' => '1234567890',
+                'email' => 'admin@admin.com',
+                'password' => bcrypt('12345678'),
+                'role' => 'admin',
+                'is_verified' => 'verified',
+            ],
+            [
+                'name' => 'Noor qnebi',
+                'phone' => '0503889420',
+                'email' => 'noorqnebi@gmail.com',
+                'password' => bcrypt('12345678'),
+                'role' => 'company',
+                'is_verified' => 'verified',
+            ],
         ]);
     }
 
